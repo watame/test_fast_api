9c0d0b64c6cf421b9c07a1f856502c5a65553881
watame <pandora.border@gmail.com>
2022-05-29 21:52:43 +0900

    Dockerfile内でpoetryをインストールするようにした

--- container/fastAPI/entrypoint.sh
+++ container/fastAPI/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ -z ${PIP_CACHE_DIR} ]]; then
-    # poetryのインストール
-    pip install poetry;
-fi
-
 if [ -f pyproject.toml ]; then
     # poetryで利用する環境をGlobalで利用する
     poetry config virtualenvs.in-project true
