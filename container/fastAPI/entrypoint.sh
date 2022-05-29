fde2ebed32b6c93c3dcf82351ae4ec0de208f896
watame <pandora.border@gmail.com>
2022-05-29 21:21:37 +0900

    :+1: #4 poetryを利用した開発環境に切り替え

--- container/fastAPI/entrypoint.sh
+++ container/fastAPI/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-if [[ -z ${PIP_CACHE_DIR} ]];then
-    # requirements.txt に定義されているパッケージをインストールする
-    pip install -r requirements.txt --quiet
+if [[ -z ${PIP_CACHE_DIR} ]]; then
+    # poetryのインストール
+    pip install poetry;
+fi
+
+if [ -f pyproject.toml ]; then
+    # poetryで利用する環境をGlobalで利用する
+    poetry config virtualenvs.in-project true
+    poetry install
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
