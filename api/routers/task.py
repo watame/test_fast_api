88f8fe2da672cece23b1f52d3ffb0dde215a1d70
watame <pandora.border@gmail.com>
2022-06-01 22:08:06 +0900

    関数名がコピペ丸出しだったところを更新

--- api/routers/task.py
+++ api/routers/task.py
@@ -7,18 +7,30 @@ router = APIRouter()
 # -> @app.get('/task') は/taskのパスへGETでアクセスするという意味
 @router.get('/tasks')
 async def list_tasks():
+    """
+    TODOタスクの一覧を取得する
+    """
     # 後で処理を追加するのでpassを記載しておく
     # pass -> 何もしないことを明示する書き方
     pass
 
 @router.post('/tasks')
-async def list_tasks():
+async def create_task():
+    """
+    新たなTODOタスクを作成する
+    """
     pass
 
 @router.put('/tasks/{task_id}')
-async def list_tasks():
+async def update_task():
+    """
+    IDが合致する既存のTODOタスクを更新する
+    """
     pass
 
 @router.delete('/tasks/{task_id}')
-async def list_tasks():
+async def delete_task():
+    """
+    IDが合致する既存のTODOタスクを削除する
+    """
     pass
