23ad97b1beb6f001aca440e5737803a10783f324
watame <pandora.border@gmail.com>
2022-06-01 22:08:28 +0900

    OpenAPIドキュメントでも何をしているのかわかるようDocを追加

--- api/routers/done.py
+++ api/routers/done.py
@@ -7,10 +7,16 @@ router = APIRouter()
 # -> @app.get('/tasks/{task_id}/done') は/task/1/done のようなパスへGETでアクセスするという意味
 @router.put('/tasks/{task_id}/done')
 async def check_task_as_done():
+    """
+    IDが合致する既存のTODOタスクを完了状態にする
+    """
     # 後で処理を追加するのでpassを記載しておく
     # pass -> 何もしないことを明示する書き方
     pass
 
 @router.delete('/tasks/{task_id}/done')
 async def uncheck_task_as_done():
+    """
+    IDが合致する既存のTODOタスクを未完了状態にする
+    """
     pass
