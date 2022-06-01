0b1a52f4bb370328b8169fcc7bf389805c8da42e
watame <pandora.border@gmail.com>
2022-06-01 21:53:49 +0900

    ダブルクォートをシングルクォートに統一

--- api/routers/task.py
+++ api/routers/task.py
@@ -4,20 +4,21 @@ router = APIRouter()
 
 # @で始まるデコレーターで関数に対してパスオペレーションの処理を追加する
 # @app.operation(path) の形式で定義する
-# -> @app.get("/task") は/taskのパスへGETでアクセスするという意味
-@router.get("/tasks")
+# -> @app.get('/task') は/taskのパスへGETでアクセスするという意味
+@router.get('/tasks')
 async def list_tasks():
     # 後で処理を追加するのでpassを記載しておく
+    # pass -> 何もしないことを明示する書き方
     pass
 
-@router.post("/tasks")
+@router.post('/tasks')
 async def list_tasks():
     pass
 
-@router.put("/tasks/{task_id}")
+@router.put('/tasks/{task_id}')
 async def list_tasks():
     pass
 
-@router.delete("/tasks/{task_id}")
+@router.delete('/tasks/{task_id}')
 async def list_tasks():
     pass
