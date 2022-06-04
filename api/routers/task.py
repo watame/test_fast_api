6a7ac5aac03b8433fb63f3c7ff66cb96e7c9657d
watame <pandora.border@gmail.com>
2022-06-04 22:08:46 +0900

    TODO作成のスキーマを定義

--- api/routers/task.py
+++ api/routers/task.py
@@ -18,12 +18,21 @@ async def list_tasks():
     # デフォルト値が入っているdoneは指定しなくてもOK
     return [task_schema.Task(id=1, title='1つ目のTODOタスク')]
 
-@router.post('/tasks')
-async def create_task():
+# response_modelにレスポンスとして戻したい値の定義を設定
+@router.post('/tasks', response_model=task_schema.TaskCreateResponse)
+async def create_task(task_body: task_schema.TaskCreate):
     """
     新たなTODOタスクを作成する
+
+    Args:
+        task_body:
+            task_schema.TaskCreateのインスタンス
     """
-    pass
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+    # dictインスタンスに対して先頭に**を付けることでdictをキーワード引数として展開できる
+    # つまり、以下の挙動と同義になる
+    # -> return task_schema.TaskCreateResponse(id=1, title=task_body.title, done=task_body.done)
+
 
 @router.put('/tasks/{task_id}')
 async def update_task():
--- api/schemas/task.py
+++ api/schemas/task.py
@@ -2,14 +2,42 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 # FastAPIのスキーマモデルの元となるBaseModelを継承してスキーマを作成する
-class Task(BaseModel):
+class TaskBase(BaseModel):
     """
-    TODO情報
+    TODO基底スキーマ
+    共通で利用するフィールド定義を設定し、個別のTODOスキーマはこのクラスを継承して定義する
     """
-    # TypeHintとしてintを指定
-    id: int
     # Noneを許容する型のOptionalを利用して、値が入らない可能性を明示する
     # 右辺のFieldは(デフォルト値, オプション設定)のようにして利用する
     # -> Field(None, example='xxx') であれば、OpenAPIのExampleの値に'xxx'が設定される
     title: Optional[str] = Field(None, example='クリーニングを取りに行く')
+
+class TaskCreate(TaskBase):
+    """
+    TODO作成スキーマ
+    """
+    # 作成時は「IDは自動採番」「完了フラグも作成時は常にFalse」なので明示的に定義せず、継承した情報のみを利用する
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    """
+    TODO作成リクエストのレスポンススキーマ
+    """
+    id: int
+
+    # DB接続の際に利用するクラス
+    class Config:
+        orm_mode = True
+
+
+class Task(TaskBase):
+    """
+    TODO詳細スキーマ
+    """
+    # TypeHintとしてintを指定
+    id: int
     done: bool = Field(False, description='完了フラグ')
+
+    # DB接続の際に利用するクラス
+    class Config:
+        orm_mode = True
