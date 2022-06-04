99d4856d08e74694b288c2d418d3c28efb9cef41
watame <pandora.border@gmail.com>
2022-06-04 22:18:06 +0900

    TODO作成スキーマのルーティング定義を追加

--- api/routers/task.py
+++ api/routers/task.py
@@ -34,16 +34,27 @@ async def create_task(task_body: task_schema.TaskCreate):
     # -> return task_schema.TaskCreateResponse(id=1, title=task_body.title, done=task_body.done)
 
 
-@router.put('/tasks/{task_id}')
-async def update_task():
+@router.put('/tasks/{task_id}', response_model=task_schema.TaskCreateResponse)
+async def update_task(task_id: int, task_body: task_schema.TaskCreate):
     """
     IDが合致する既存のTODOタスクを更新する
-    """
-    pass
 
-@router.delete('/tasks/{task_id}')
-async def delete_task():
+    Args:
+        task_id:
+            URIに含まれるタスクインスタンスのID
+        task_body:
+            task_schema.TaskCreateのインスタンス
+    """
+    return task_schema.TaskCreateResponse(id=task_id, **task_body.dict())
+
+@router.delete('/tasks/{task_id}', response_model=None)
+async def delete_task(task_id: int):
     """
     IDが合致する既存のTODOタスクを削除する
+
+    Args:
+        task_id:
+            URIに含まれるタスクインスタンスのID
     """
-    pass
+    # 現状、削除するものがないのでReturnするだけ
+    return
