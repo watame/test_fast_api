ad9053b897119bb27ed844a61b99e2502fbb4f8d
watame <pandora.border@gmail.com>
2022-06-12 22:10:52 +0900

    テストで発生していた例外へ対処した（仮）

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,6 +20,8 @@ TEST_DB_URL = URL.create(
     query={'charset': 'utf8mb4'}
 )
 
+# スコープ
+# https://qiita.com/_akiyama_/items/9ead227227d669b0564e#%E3%82%B9%E3%82%B3%E3%83%BC%E3%83%97
 @pytest.fixture
 async def async_client() -> AsyncClient:
     """
@@ -56,6 +58,13 @@ async def async_client() -> AsyncClient:
     # テスト用に非同期HTTPクライアントを返却
     async with AsyncClient(app=app, base_url='http://test') as client:
         yield client
+
+    # テストが終わったのちに接続を終了する
+    # https://docs.sqlalchemy.org/en/14/core/connections.html#sqlalchemy.engine.Engine.dispose
+    # -> 接続を終了しないと「RuntimeError: Event loop is closed」が発出される
+    #    https://stackoverflow.com/questions/40420243/event-loop-is-closed-error-in-aiomysql
+    # TODO: 最初セッションを作成するようにして、毎回コネクションを作成しないようにする
+    await async_engine.dispose()
 
 # asyncioデコレーターで非同期関数のPytest
 @pytest.mark.asyncio
@@ -103,21 +112,3 @@ async def test_done_flag(async_client):
     # 完了フラグが外れているので404エラーとなる
     response = await async_client.delete('/tasks/1/done')
     assert response.status_code == starlette.status.HTTP_404_NOT_FOUND
-
-# このテストを実行すると下記のエラーが発生するため、fixtureの終了処理として `async_engine.close()` をする必要がある
-# https://stackoverflow.com/questions/40420243/event-loop-is-closed-error-in-aiomysql
-"""
-Exception ignored in: <function Connection.__del__ at 0x7fc9d0dbec20>
-Traceback (most recent call last):
-  File "/fastapi/.venv/lib/python3.10/site-packages/aiomysql/connection.py", line 1120, in __del__
-    self.close()
-  File "/fastapi/.venv/lib/python3.10/site-packages/aiomysql/connection.py", line 339, in close
-    self._writer.transport.close()
-  File "/usr/local/lib/python3.10/asyncio/selector_events.py", line 698, in close
-    self._loop.call_soon(self._call_connection_lost, None)
-  File "/usr/local/lib/python3.10/asyncio/base_events.py", line 750, in call_soon
-    self._check_closed()
-  File "/usr/local/lib/python3.10/asyncio/base_events.py", line 515, in _check_closed
-    raise RuntimeError('Event loop is closed')
-RuntimeError: Event loop is closed
-"""
