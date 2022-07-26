5646a4b248ac31f92cd84a76a72effd54f100d1c
watame <pandora.border@gmail.com>
2022-07-26 22:49:41 +0900

    CORS設定を追加

--- api/main.py
+++ api/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers import task, done
 
 # uvicornを通じて参照されるインスタンス
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 定義したルーティング定義を読み込み、エンドポイントを追加する
 app.include_router(task.router)
